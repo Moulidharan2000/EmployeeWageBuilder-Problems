eb732da674a3dee2d9232d8ca6b81e97386145aa
Moulidharan <smoulidharanmech@gmail.com>
2022-11-25 17:10:07 +0530

    add : Manage EmployeeWages For Multiple Companies

--- src/main/java/com/employeeWages/EmployeeWages.java
+++ src/main/java/com/employeeWages/EmployeeWages.java
@@ -1,16 +1,38 @@
-package com.employeeWages;
+package com.employeeWagesUC;
 
-public class EmployeeWages {
+public class employeeWages {
 
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		EmployeeWageCompute company1 = new EmployeeWageCompute();
-		EmployeeWageCompute company2 = new EmployeeWageCompute();
-		
-		
+		// declaring instance variables
+	    final String COMPANY_NAME;
+	    final int WAGE_PER_HR;
+	    final int MAX_WORKING_DAYS;
+	    final int MAX_WORKING_HRS;
+	  
+	  //Constructor
+	    
+	    CompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
+	        String COMPANYNAME = companyName;
+	        int WAGEPERHR = wagePerHr;
+	        int MAXWORKINGDAYS = maxWorkingDays;
+	        int MAXWORKINGHRS = maxWorkingHrs;
+	        int totalEmpWage = 0;
+	    }
+	   void setTotalEmployeeWage(int totalEmpWage) {
+	        this.totalEmpWage = totalEmpWage;
+	    }
+	    public String toString() {
+	        System.out.println("                                                             ");
+	        System.out.println("* Total Information of " + COMPANY_NAME + " employee");
+	        System.out.println("Wage per hour:" + WAGE_PER_HR);
+	        System.out.println("Maximum working days:" + MAX_WORKING_DAYS);
+	        System.out.println("Maximum working hours:" + MAX_WORKING_HRS);
+	        return "Total wage in a month for " + COMPANY_NAME + " Company is " + totalEmpWage + "\n";
+		}
 
 	}
 
-}
+
+	}
+
